149c479b5c3028cc4fc6104a2f508be643ff56c3
Zeny97 <JustinSontopski@outlook.de>
2024-01-12 17:40:11 +0100

    Restructured file locations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,56 +7,23 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float movespeed = 7f;
-    Vector2 inputVector = new Vector2 (0, 0);
-
+    [SerializeField] private GameInput gameInput;
+    private bool isWalking;
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
 
-        // Player has the same magnitude in all directions including diagonal
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDirection * movespeed * Time.deltaTime;
-        transform.forward = moveDirection;
+
+        isWalking = moveDirection != Vector3.zero;
+
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
     }
 
-    //private void FixedUpdate()
-    //{
-    //    Move();
-    //}
-
-    //private void Move()
-    //{
-    //}
-
-    //private void Look()
-    //{
-
-    //}
-
-    //public void OnMove(InputAction.CallbackContext context)
-    //{
-    //    move = context.ReadValue<Vector2>();
-    //}
-
-    //public void OnLook(InputAction.CallbackContext context)
-    //{
-    //    look = context.ReadValue<Vector2>();
-    //}
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
 }
